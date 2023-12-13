cc901acf30b6adef84694fdd52b40702e1c8f961
Vinicius Mendes <vinimendesdev@gmail.com>
2023-12-13 14:12:37 -0300

    feat: added home page guard

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,27 @@
-export default function Home() {
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+import { cookies } from 'next/headers';
+import { RedirectType, redirect } from 'next/navigation';
+
+export default async function Home() {
+  let loggedIn = false;
+
+  try {
+    const supabase = createServerComponentClient({ cookies });
+
+    const {
+      data: { session },
+    } = await supabase.auth.getSession();
+
+    if (session) loggedIn = true;
+  } catch (error) {
+    console.log('Home', error);
+  } finally {
+    if (loggedIn) redirect('/user-app', RedirectType.replace);
+  }
+
   return (
     <main>
-      <p>Page Home</p>
+      <p className=''>Page Home</p>
     </main>
   );
 }
