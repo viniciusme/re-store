b25abe2de9043514575fad3cb2d0ab0f750bff47
Vinicius Mendes <vinimendesdev@gmail.com>
2023-12-14 15:23:40 -0300

    feat: added tabs component to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from 'next/headers';
 import { RedirectType, redirect } from 'next/navigation';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import CreateAccountForm from '@/components/auth/create-account-form';
 
 export default async function Home() {
   let loggedIn = false;
@@ -20,8 +22,27 @@ export default async function Home() {
   }
 
   return (
-    <main>
-      <p className=''>Page Home</p>
-    </main>
+    <div className='flex flex-col h-screen w-full justify-center items-center'>
+      <Tabs
+        defaultValue='create-account'
+        className='w-[400px] border rounded-md pb-4 shadow-2xl'
+      >
+        <TabsList className='flex justify-around items-center rounded-b-none h-14'>
+          <TabsTrigger
+            value='create-account'
+            className='transition-all delay-150'
+          >
+            Account
+          </TabsTrigger>
+          <TabsTrigger value='login' className='transition-all delay-150'>
+            Login
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value='create-account'>
+          <CreateAccountForm />
+        </TabsContent>
+        <TabsContent value='login'></TabsContent>
+      </Tabs>
+    </div>
   );
 }
