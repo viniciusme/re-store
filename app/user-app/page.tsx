4f6a4204d1f968a1796c5d1bab302173dabdad53
Vinicius Mendes <vinimendesdev@gmail.com>
2023-12-22 10:49:36 -0300

    feat: ensuring verification of whether the user is logged in or not.

--- app/user-app/page.tsx
+++ app/user-app/page.tsx
@@ -1,4 +1,25 @@
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+import { RedirectType } from 'next/dist/client/components/redirect';
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
+
 const UserApp = async () => {
+  let loggedIn = false;
+
+  try {
+    const supabase = createServerComponentClient({ cookies });
+
+    const {
+      data: { session },
+    } = await supabase.auth.getSession();
+
+    if (session) loggedIn = true;
+  } catch (error) {
+    console.log('UserApp', error);
+  } finally {
+    if (!loggedIn) redirect('/', RedirectType.replace);
+  }
+
   return (
     <div>
       <p>User Logged in</p>
