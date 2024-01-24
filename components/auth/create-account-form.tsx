49c67271953b2098578cb2ad2204ddaff790414c
Vinicius Mendes <vinimendesdev@gmail.com>
2024-01-24 20:26:46 -0300

    feat: added CreateAccountForm component

--- components/auth/create-account-form.tsx
+++ components/auth/create-account-form.tsx
@@ -1,11 +1,11 @@
-'use client';
+"use client";
 
-import * as z from 'zod';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useForm } from 'react-hook-form';
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import {
   Form,
   FormControl,
@@ -14,27 +14,27 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from '@/components/ui/form';
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
-import { useRouter } from 'next/navigation';
+} from "@/components/ui/form";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useRouter } from "next/navigation";
 
 const formSchema = z.object({
   email: z
     .string({
-      required_error: 'Email is required',
+      required_error: "Email is required",
     })
     .email({
-      message: 'Please enter a valid email',
+      message: "Please enter a valid email",
     }),
   password: z
     .string({
-      required_error: 'Password is required',
+      required_error: "Password is required",
     })
     .min(8, {
-      message: 'Password must be at least 8 characters',
+      message: "Password must be at least 8 characters",
     })
     .max(100, {
-      message: 'Password must be less than 100 characters',
+      message: "Password must be less than 100 characters",
     }),
 });
 
@@ -44,8 +44,8 @@ const CreateAccountForm = () => {
     resolver: zodResolver(formSchema),
 
     defaultValues: {
-      email: '',
-      password: '',
+      email: "",
+      password: "",
     },
   });
 
@@ -76,22 +76,22 @@ const CreateAccountForm = () => {
   };
 
   return (
-    <div className='flex flex-col justify-center items-center space-y-2'>
-      <span className='text-lg'>Tou will love it.</span>
+    <div className="flex flex-col items-center justify-center space-y-2">
+      <span className="text-lg">Tou will love it.</span>
 
       <Form {...form}>
         <form
           onSubmit={form.handleSubmit(onSubmit)}
-          className='flex flex-col space-y-2'
+          className="flex flex-col space-y-2"
         >
           <FormField
             control={form.control}
-            name='email'
+            name="email"
             render={({ field }) => (
               <FormItem>
                 <FormLabel>E-mail</FormLabel>
                 <FormControl>
-                  <Input type='email' placeholder='E-mail' {...field} />
+                  <Input type="email" placeholder="E-mail" {...field} />
                 </FormControl>
                 <FormDescription>This is your E-mail</FormDescription>
                 <FormMessage />
@@ -101,12 +101,12 @@ const CreateAccountForm = () => {
 
           <FormField
             control={form.control}
-            name='password'
+            name="password"
             render={({ field }) => (
               <FormItem>
                 <FormLabel>Password</FormLabel>
                 <FormControl>
-                  <Input type='password' placeholder='password' {...field} />
+                  <Input type="password" placeholder="password" {...field} />
                 </FormControl>
                 <FormDescription>This is your password</FormDescription>
                 <FormMessage />
@@ -114,7 +114,7 @@ const CreateAccountForm = () => {
             )}
           />
 
-          <Button type='submit'>Create Account</Button>
+          <Button type="submit">Create Account</Button>
         </form>
       </Form>
     </div>
