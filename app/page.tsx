888e3817e03fcfab1568bf7adf06e98484aef52a
Vinicius Mendes <vinimendesdev@gmail.com>
2024-01-24 20:32:27 -0300

    style: made improvements to the Home file styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
-import { RedirectType } from 'next/dist/client/components/redirect';
-import { cookies } from 'next/headers';
-import { redirect } from 'next/navigation';
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { RedirectType } from "next/dist/client/components/redirect";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
-import CreateAccountForm from '@/components/auth/create-account-form';
-import LoginAccountForm from '@/components/auth/login.account-form';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import CreateAccountForm from "@/components/auth/create-account-form";
+import LoginAccountForm from "@/components/auth/login.account-form";
 
 export default async function Home() {
   let loggedIn = false;
@@ -19,32 +19,32 @@ export default async function Home() {
 
     if (session) loggedIn = true;
   } catch (error) {
-    console.log('Home', error);
+    console.log("Home", error);
   } finally {
-    if (loggedIn) redirect('/user-app', RedirectType.replace);
+    if (loggedIn) redirect("/user-app", RedirectType.replace);
   }
 
   return (
-    <div className='flex flex-col h-screen w-full justify-center items-center'>
+    <div className="flex h-screen w-full flex-col items-center justify-center">
       <Tabs
-        defaultValue='create-account'
-        className='w-[400px] border rounded-md pb-4 shadow-2xl'
+        defaultValue="create-account"
+        className="w-[400px] rounded-md border pb-4 shadow-2xl"
       >
-        <TabsList className='flex justify-around items-center rounded-b-none h-14'>
+        <TabsList className="flex h-14 items-center justify-around rounded-b-none">
           <TabsTrigger
-            value='create-account'
-            className='transition-all delay-150'
+            value="create-account"
+            className="transition-all delay-150"
           >
             Account
           </TabsTrigger>
-          <TabsTrigger value='login' className='transition-all delay-150'>
+          <TabsTrigger value="login" className="transition-all delay-150">
             Login
           </TabsTrigger>
         </TabsList>
-        <TabsContent value='create-account'>
+        <TabsContent value="create-account">
           <CreateAccountForm />
         </TabsContent>
-        <TabsContent value='login'>
+        <TabsContent value="login">
           <LoginAccountForm />
         </TabsContent>
       </Tabs>
