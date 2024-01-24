ac68cb9751f03a25a824612ec8d1d6492a11a8fc
Vinicius Mendes <vinimendesdev@gmail.com>
2024-01-24 20:30:30 -0300

    feat: added router middleware

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,10 +1,10 @@
-import { NextRequest, NextResponse } from 'next/server';
-import { cookies } from 'next/headers';
-import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
+import { NextRequest, NextResponse } from "next/server";
+import { cookies } from "next/headers";
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 
 export async function GET(request: NextRequest) {
   const requestUrl = new URL(request.url);
-  const code = requestUrl.searchParams.get('code');
+  const code = requestUrl.searchParams.get("code");
 
   try {
     if (code) {
@@ -12,7 +12,7 @@ export async function GET(request: NextRequest) {
       await supabase.auth.exchangeCodeForSession(code);
     }
   } catch (error) {
-    console.log('Auth_Callback', error);
+    console.log("Auth_Callback", error);
   }
 
   return NextResponse.redirect(requestUrl.origin);
