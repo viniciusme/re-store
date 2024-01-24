4ce6c44125f744ba14de1673c90af7ffe99a0488
Vinicius Mendes <vinimendesdev@gmail.com>
2024-01-24 20:26:26 -0300

    feat: added UserNav component

--- components/common/user-nav.tsx
+++ components/common/user-nav.tsx
@@ -1,15 +1,15 @@
-'use client';
+"use client";
 
 import {
   User,
   createClientComponentClient,
-} from '@supabase/auth-helpers-nextjs';
-import { useState, useEffect } from 'react';
-import { useRouter } from 'next/navigation';
-import Link from 'next/link';
-import { UserIcon } from 'lucide-react';
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
-import { Button } from '@/components/ui/button';
+} from "@supabase/auth-helpers-nextjs";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+import { UserIcon } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -19,9 +19,9 @@ import {
   DropdownMenuSeparator,
   DropdownMenuShortcut,
   DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
+} from "@/components/ui/dropdown-menu";
 
-import UserAvatar from '/public/images/avatars/man.png';
+import UserAvatar from "/public/images/avatars/man.png";
 
 export function UserNav() {
   const [user, setUser] = useState<User | null>();
@@ -35,7 +35,7 @@ export function UserNav() {
     } = await supabase.auth.getUser();
 
     if (error) {
-      console.log('UserNav: ', error);
+      console.log("UserNav: ", error);
     } else {
       setUser(user);
     }
@@ -56,21 +56,21 @@ export function UserNav() {
       {user && (
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
-            <Button variant='ghost' className='relative h-8 w-8 rounded-full'>
-              <Avatar className='h-8 w-8'>
-                <AvatarImage src={UserAvatar.src} alt='@reStore' />
+            <Button variant="ghost" className="relative h-8 w-8 rounded-full">
+              <Avatar className="h-8 w-8">
+                <AvatarImage src={UserAvatar.src} alt="@reStore" />
                 <AvatarFallback>RS</AvatarFallback>
               </Avatar>
             </Button>
           </DropdownMenuTrigger>
-          <DropdownMenuContent className='w-56' align='end' forceMount>
-            <DropdownMenuLabel className='font-normal'>
-              <div className='flex flex-col space-y-1'>
-                <p className='text-sm font-medium leading-none'>
-                  {' '}
-                  {user.email?.split('@')[0]}
+          <DropdownMenuContent className="w-56" align="end" forceMount>
+            <DropdownMenuLabel className="font-normal">
+              <div className="flex flex-col space-y-1">
+                <p className="text-sm font-medium leading-none">
+                  {" "}
+                  {user.email?.split("@")[0]}
                 </p>
-                <p className='text-xs leading-none text-muted-foreground'>
+                <p className="text-xs leading-none text-muted-foreground">
                   {user.email}
                 </p>
               </div>
